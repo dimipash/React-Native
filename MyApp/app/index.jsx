99cc8738171f9a3587f7ff1f09f1281fdbf559ea
Dimitar Pashev <dim.pashev@gmail.com>
2025-04-09 14:47:11 +0300

    build list views

--- MyApp/app/index.jsx
+++ MyApp/app/index.jsx
@@ -27,6 +27,15 @@ const app = () => {
                         <Text style={styles.buttonText}>Contact Us</Text>
                     </Pressable>
                 </Link>
+                <Link
+                    href="/menu"
+                    style={{ marginHorizontal: "auto" }}
+                    asChild
+                >
+                    <Pressable style={styles.button}>
+                        <Text style={styles.buttonText}>Our Menu</Text>
+                    </Pressable>
+                </Link>
             </ImageBackground>
         </View>
     );
@@ -65,11 +74,13 @@ const styles = StyleSheet.create({
     },
     button: {
         height: 60,
+        width: 150,
         borderRadius: 20,
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "rgba(0, 0, 0, 0.75)",
         padding: 6,
+        marginBottom: 50,
     },
     buttonText: {
         color: "white",
--- MyApp/app/menu.jsx
+++ MyApp/app/menu.jsx
@@ -23,18 +23,42 @@ export default function MenuScreen() {
 
     const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
 
+    const separatorComp = <View style={styles.separator} />;
+
+    const headerComp = <Text>Top Of List</Text>;
+    const footerComp = <Text style={{color: theme.text}}>End Of Menu</Text>;
+
     return (
         <Container>
             <FlatList
                 data={MENU_ITEMS}
                 keyExtractor={(item) => item.id.toString()}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={styles.contentContainer}
+                ItemSeparatorComponent={separatorComp}
+                // ListHeaderComponent={headerComp}
+                ListFooterComponent={footerComp}
+                ListFooterComponentStyle={styles.footerComp}
+                ListEmptyComponent={<Text>No Items</Text>}
                 renderItem={({ item }) => (
-                    <View>
-                        <View>
-                            <Text>{item.title}</Text>
-                            <Text>{item.desription}</Text>
+                    <View style={styles.row}>
+                        <View style={styles.menuTextRow}>
+                            <Text
+                                style={[
+                                    styles.menuItemText,
+                                    styles.menuItemTitle,
+                                ]}
+                            >
+                                {item.title}
+                            </Text>
+                            <Text style={styles.menuItemText}>
+                                {item.desription}
+                            </Text>
                         </View>
-                        <Image source={MENU_IMAGES[item.id - 1]} />
+                        <Image
+                            source={MENU_IMAGES[item.id - 1]}
+                            style={styles.menuImage}
+                        />
                     </View>
                 )}
             />
@@ -43,5 +67,54 @@ export default function MenuScreen() {
 }
 
 function createStyles(theme, colorScheme) {
-    return StyleSheet.create({});
+    return StyleSheet.create({
+        contentContainer: {
+            paddingTop: 10,
+            paddingBottom: 20,
+            paddingHorizontal: 12,
+            backgroundColor: theme.background,
+        },
+        separator: {
+            height: 1,
+            backgroundColor: colorScheme === "dark" ? "papayawhip" : "#000",
+            width: "50%",
+            maxWidth: 300,
+            marginHorizontal: "auto",
+            marginBottom: 10,
+        },
+        footerComp: {            
+            marginHorizontal: "auto",
+        },
+        row: {
+            flexDirection: "row",
+            width: "100%",
+            maxWidth: 600,
+            height: 100,
+            marginBottom: 10,
+            borderStyle: "solid",
+            borderColor: colorScheme === "dark" ? "papayawhip" : "#000",
+            borderWidth: 1,
+            borderRadius: 20,
+            overflow: "hidden",
+            marginHorizontal: "auto",
+        },
+        menuTextRow: {
+            width: "65%",
+            paddingTop: 10,
+            paddingLeft: 10,
+            paddingRight: 5,
+            flexGrow: 1,
+        },
+        menuItemTitle: {
+            fontSize: 18,
+            textDecorationLine: "underline",
+        },
+        menuItemText: {
+            color: theme.text,
+        },
+        menuImage: {
+            width: 100,
+            height: 100,
+        }
+    });
 }
