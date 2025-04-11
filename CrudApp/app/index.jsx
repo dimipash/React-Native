6bc1fcc52a4cac336e9f1c4b257a8c13974e4b25
Dimitar Pashev <dim.pashev@gmail.com>
2025-04-11 19:17:08 +0300

    add todo functions

--- CrudApp/app/index.jsx
+++ CrudApp/app/index.jsx
@@ -1,15 +1,38 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
+
+import { data } from "@/data/todos";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const [todos, setTodos] = useState(data.sort((a, b) => b.id - a.id));
+    const [text, setText] = useState("");
+
+    const addTodo = () => {
+        if (text.trim()) {
+            const newId = todos.length > 0 ? todos[0].id + 1 : 1;
+            setTodos([{ id: newId, title: text, completed: false }, ...todos]);
+            setText("");
+        }
+    };
+
+    const toggleTodo = (id) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, completed: !todo.completed } : todo
+            )
+        );
+    };
+
+    const removeTodo = (id) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    return (
+        <SafeAreaView>
+            <View>
+                <TextInput></TextInput>
+            </View>
+        </SafeAreaView>
+    );
 }
